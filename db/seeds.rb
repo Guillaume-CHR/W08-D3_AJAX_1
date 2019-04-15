0d6889cb6dfa0db7985541886829f32ad19a12a8
guillaume-chr <guillaume.christe@gmail.com>
2019-04-15 20:40:42 +0200

    Creation of the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'bundler'
+Bundler.require
+
+5.times do 
+	Book.create(author: Faker::Book.author, title: Faker::Book.title)
+end
